abc1f4be28d8a0fc5aa6a7f286ebc5b14019c02a
Anders Eriksson <digitaldoktorn@gmail.com>
2020-09-18 11:41:31 +0200

    50 and 51 googlemaps and campus cpt

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -47,6 +47,8 @@ function university_files()
     wp_enqueue_style('custom-google-fonts', '/fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyAn6kb_jBGFk4OQQ3MeeP3-Neak1CBGQKM', NULL, '1.0', true);
+
     if(strstr($_SERVER['SERVER_NAME'], 'localhost')) {
         wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
     } else {
@@ -97,3 +99,10 @@ function university_adjust_queries($query) {
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+function universityMapKey($api) {
+    $api['key'] = 'AIzaSyAn6kb_jBGFk4OQQ3MeeP3-Neak1CBGQKM';
+    return $api;
+}
+add_filter('acf/fields/google_map/api', 'universityMapKey');
+
