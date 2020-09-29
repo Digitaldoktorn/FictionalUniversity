b0532933304554669c28a52e7dd75c33b717f782
Anders Eriksson <digitaldoktorn@gmail.com>
2020-09-29 15:56:13 +0200

    55 campuses continued

--- themes/fictional-university-theme/archive-campus.php
+++ themes/fictional-university-theme/archive-campus.php
@@ -18,12 +18,12 @@ pageBanner([
         $mapLocation = get_field('map_location');
         
         ?>
-        <div class="marker" data-lat="<?php echo mapLocation['lat'] ?>" data-lng="<?php echo mapLocation['lng'] ?>">        
+        <div class="marker" data-lat="<?php echo mapLocation['lat'] ?>" data-lng="<?php echo mapLocation['lng'] ?>">
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <?php echo $mapLocation['address']; ?>
         </div>
         
-    <?php }
-        echo paginate_links(); 
-    ?>
+    <?php }?>
     </div>
     </div>
 
